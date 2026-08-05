95a77c7056407f6dffef3ae5924d75cf9ea9058d
Zafrin Binte Ali Tutul <zafrintutul9@gmail.com>
2026-08-05 14:11:58 +0000

    main

--- main.sh
+++ main.sh
@@ -1,44 +1,29 @@
 #!/bin/bash
 
+# ====================================
+# Process Scheduling Simulator
+# Main Controller
+# ====================================
 
-# Load algorithm files
+# Algorithm files
 
-source algorithms/calculations.sh
+source algorithms/calculation.sh
 source algorithms/fcfs.sh
 source algorithms/sjf.sh
 source algorithms/priority.sh
 source algorithms/round_robin.sh
 
+# Visualization files
 
-run_algorithm() {
+source visualization/colors.sh
+source visualization/validation.sh
+source visualization/display.sh
+source visualization/gantt.sh
+source visualization/help.sh
+source visualization/save_output.sh
+source visualization/compare.sh
+source visualization/menu.sh
 
-    local algorithm=$1
-    local n=$2
-    local quantum=$3
+# Start the program
 
-    case "$algorithm" in
-
-        1)
-            run_fcfs "$n"
-            ;;
-
-        2)
-            run_sjf "$n"
-            ;;
-
-        3)
-            run_priority "$n"
-            ;;
-
-        4)
-            run_round_robin "$n" "$quantum"
-            ;;
-
-        *)
-            echo "Invalid Algorithm"
-            return 1
-            ;;
-
-    esac
-
-}
+main_menu
